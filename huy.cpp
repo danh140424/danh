9ee3a9ef8f1dd2ea13850fce6809713787663afe
danh140424 <162082161+danh140424@users.noreply.github.com>
2024-04-01 10:40:07 +0700

    Add files via upload

---
+++ huy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+	 int a, n;
+	 int s = 1;
+	 cout << " no a :";
+	 cin >> a;
+	 cout << endl;
+	 cout << "no n:";
+	 cin >> n;
+	 cout << endl;
+	 for (int index = 0; index < n; index++);
+	 s = a + s;
+	 cout << "ssss" << s << endl;
+	 return 0;
+}
