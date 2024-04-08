a710f434d44e64c2697daafe51cd9031e7262d03
danh140424 <162082161+danh140424@users.noreply.github.com>
2024-04-08 09:57:06 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a;
+    int S = 1;
+    cout << "input a:";
+    cin >> a;
+    cout << endl;
+    cout << "input n:";
+    cin >> n;
+    cout << endl;
+    for (int index = 0; index < n; index++);
+    {//(n-1)=n lần 
+        S = a * 5;
+    }
+    cout << "ontput s lũy thừa" << S << endl;
+    return 0;
+
+
+}
